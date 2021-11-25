0284d566d272e6d32148abb307e8a54db73338ef
hoangdv99 <hoangdv257@gmail.com>
2021-11-25 22:07:39 +0700

    add ads provider to all routes

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import EditProduct from "../pages/products/edit"
 import Advertisements from '../pages/advertisements'
 import NewAdvertisement from '../pages/advertisements/new'
 import { ProductProvider } from "../contexts/ProductContext"
+import { AdvertisementProvider } from "../contexts/AdvertisementContext"
 
 function App() {
   return (
@@ -47,6 +48,34 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
           </Routes>
+          <AdvertisementProvider>
+            <Routes>
+              <Route path='/' element={<PrivateRoute />}>
+                <Route path='/' element={<Dashboard />}/>
+              </Route>
+              <Route path='/update-profile' element={<PrivateRoute />}>
+                <Route path='/update-profile' element={<UpdateProfile />}/>
+              </Route>
+              <Route path='/products' element={<PrivateRoute />}>
+                <Route path='/products' element={<Products />}/>
+              </Route>
+              <Route path='/product/add' element={<PrivateRoute />}>
+                <Route path='/product/add' element={<NewProduct />}/>
+              </Route>
+              <Route path='/product/:id/edit' element={<PrivateRoute />}>
+                <Route path='/product/:id/edit' element={<EditProduct />}/>
+              </Route>
+              <Route path='/advertisements' element={<PrivateRoute />}>
+                <Route path='/advertisements' element={<Advertisements />}/>
+              </Route>
+              <Route path='/advertisement/add' element={<PrivateRoute />}>
+                <Route path='/advertisement/add' element={<NewAdvertisement />}/>
+              </Route>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Routes>
+          </AdvertisementProvider>
         </ProductProvider>
       </AuthProvider>
     </Router>
