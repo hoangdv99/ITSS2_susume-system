36d9c33f46b88c51beeb67c5ecddbb855c7f0993
hoangdv99 <hoangdv257@gmail.com>
2021-12-07 22:53:25 +0700

    add toasify and change logo

--- src/components/advertisement/table.js
+++ src/components/advertisement/table.js
@@ -1,11 +1,37 @@
 import { Table, Button } from 'react-bootstrap'
 import { useAdvertisement } from '../../contexts/AdvertisementContext'
 import { Link } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify'
 
 export default function AdvertisementList() {
   const { advertisements, deleteAdvertisement } = useAdvertisement()
+  const remove = (id) => {
+    if (window.confirm('Are you sure to delete this item?')) {
+      deleteAdvertisement(id)
+      toast.success('Deleted successfully', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  } 
   return (
     <Table striped bordered hover>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      /><ToastContainer />
       <thead>
         <tr>
           <th className="text-center">ID</th>
@@ -26,7 +52,7 @@ export default function AdvertisementList() {
                 <Button variant="success"style={{marginRight:"10px", textColor:"white"}}>
                   <Link to={`/advertisement/${advertisement.id}/edit`} style={{textDecoration:"none", color: '#FFF'}}>修正</Link>
                 </Button>
-                <Button variant="danger" onClick={()=>{deleteAdvertisement(advertisement.id)}}>削除</Button>
+                <Button variant="danger" onClick={() => remove(advertisement.id)}>削除</Button>
               </td>
             </tr>
           ))
--- src/components/product/table.js
+++ src/components/product/table.js
@@ -1,11 +1,38 @@
 import { Table, Button } from 'react-bootstrap'
 import { useProduct } from '../../contexts/ProductContext'
 import { Link } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify'
 
 export default function ProductList() {
   const { products, deleteProduct } = useProduct()
+
+  const remove = (id) => {
+    if (window.confirm('Are you sure to delete this item?')) {
+      deleteProduct(id)
+      toast.success('Deleted successfully', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  }
   return (
     <Table striped bordered hover>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      /><ToastContainer />
       <thead>
         <tr>
           <th className="text-center">ID</th>
@@ -30,7 +57,7 @@ export default function ProductList() {
                 <Button variant="success"style={{marginRight:"10px"}}>
                   <Link to={`/product/${product.id}/edit`} style={{textDecoration:"none", color: '#FFF'}}>修正</Link>
                 </Button>
-                <Button variant="danger" onClick={()=>{deleteProduct(product.id)}}>削除</Button>
+                <Button variant="danger" onClick={() => remove(product.id)}>削除</Button>
               </td>
             </tr>
           ))
